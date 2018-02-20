2e47627846df687b6aed66da025c5db44a4ad568
Ilya Moskovkin <targix8@gmail.com>
2018-02-20 21:18:30 +0600

    initial commit

---
+++ src/components/app.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+import BookList from '../containers/book-list';
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <BookList />
+        
+      </div>
+    );
+  }
+}
